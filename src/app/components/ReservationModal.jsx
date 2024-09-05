52f4dd59001b6f99aa6371639d2bdb0078a968dd
MayraCortinez <mayrautopia@gmail.com>
2024-09-05 16:31:12 -0300

    form reservation ready

---
+++ src/app/components/ReservationModal.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import styles from '../styles/components/reservationModal.module.css';
+
+
+const ReservationForm = ({isOpen, onClose}) => {
+  const { register, handleSubmit } = useForm();
+  const [startDate, setStartDate] = useState(null);
+
+  const onSubmit = (data) => {
+    const whatsappNumber = '5491178865748'; // Número del administrador
+    const message = `Reserva confirmada para ${data.name}, ${data.people} personas el ${startDate?.toLocaleDateString()}. Comentarios: ${data.comments}`;
+    
+    const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
+    
+    window.open(whatsappUrl, '_blank'); // Abre WhatsApp en una nueva pestaña para enviar el mensaje
+  };
+
+  const filterDays = (date) => {
+    const day = date.getDay();
+    return day === 5 || day === 6 || day === 0; // Solo permitir viernes, sábado y domingo
+  };
+
+  // Si el modal no está abierto, no se renderiza
+  if (!isOpen) return null;
+
+  return (
+    <form className={styles.formContainer} onSubmit={handleSubmit(onSubmit)} onClick={onClose}>
+      <div 
+        onClick={(e) => e.stopPropagation()} // Evita cerrar el modal al hacer clic dentro del contenido
+        className={styles.modalContent}
+      >
+
+      <button className={styles.closeButton} onClick={onClose}>
+          &times;
+      </button>
+      <h3> Reservar </h3>
+      <TextField
+        id="outlined-basic"
+        label="Nombre y Apellido"
+        variant="outlined"
+        {...register('name')}
+        fullWidth
+        required
+        className={styles.formField}
+        margin='normal'
+      />
+      <TextField
+        id="outlined-basic"
+        label="Cantidad de Personas"
+        variant="outlined"
+        type="number"
+        {...register('people')}
+        fullWidth
+        required
+        style={{ marginTop: '16px' }}
+      />
+      <label style={{ marginTop: '16px', display: 'block' }}>Fecha:</label>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        dateFormat="dd/MM/yyyy"
+        placeholderText="Seleccione una fecha"
+        filterDate={filterDays}
+        minDate={new Date()}
+        customInput={
+          <TextField
+            id="outlined-basic"
+            label="Día/Mes/Año"
+            variant="outlined"
+            fullWidth
+            style={{ marginTop: '16px'}}
+          />
+        }
+      />
+      <TextField
+        id="outlined-basic"
+        label="Comentarios"
+        variant="outlined"
+        {...register('comments')}
+        fullWidth
+        multiline
+        rows={4}
+        style={{ marginTop: '16px' }}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        type="submit"
+        fullWidth
+        style={{ marginTop: '16px' }}
+      >
+        Confirmar Reserva
+      </Button>
+      </div>
+    </form>
+    
+      
+  );
+};
+
+export default ReservationForm;
--- src/app/components/Slider.jsx
+++ src/app/components/Slider.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+'use client';
+
+import React, {useState} from 'react';
 import styles from '../styles/components/slider.module.css'; 
+import ReservationModal from './ReservationModal';
 
 const Slider = () => {
+
+  // Definir el estado que controlará si el modal está abierto o cerrado
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  // Función para abrir el modal
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  // Función para cerrar el modal (esto será `onClose`)
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  
   return (
     <div className="slider">
       <div className={styles.slides}>
@@ -13,8 +31,10 @@ const Slider = () => {
       <div className={styles.sliderText}>
         <h1> Restaurante de Campo</h1>
         <h2> Disfruta del mejor entorno y gastronomía </h2>
-        <button className={styles.buttonReserve}>Reservar</button>
+        <button className={styles.buttonReserve} onClick={handleOpenModal}>Reservar</button>
       </div>
+        {/* Pasamos `isOpen` y `onClose` como props al componente del modal */}
+        <ReservationModal isOpen={isModalOpen} onClose={handleCloseModal} />
     </div>
   );
 };
